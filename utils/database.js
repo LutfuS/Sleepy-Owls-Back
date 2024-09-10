191c0afb99b65c2b0adc61cfa137e65e7cb3876e
jha <haimezjohn@gmail.com>
2024-09-10 13:17:51 +0200

    fix docker

--- utils/database.js
+++ utils/database.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose')
-const Logger = require('./logger').pino
-const Config = require('../config')
+const mongoose = require("mongoose");
+const Logger = require("./logger").pino;
+const Config = require("../config");
 
-mongoose.connection.on('connected', () => Logger.info("Connecté à la base de donnée."));
-mongoose.connection.on('open', () => Logger.info("Connection ouverte à la base de donnée."));
-mongoose.connection.on('disconnected', () => Logger.error("Déconnecter a la base de donnée."));
-mongoose.connection.on('reconnected', () => Logger.info("Reconnecté à la base de donnée."));
-mongoose.connection.on('disconnecting', () => Logger.error("Déconnection à la base de donnée."));
-mongoose.connection.on('close', () => Logger.info("Connection à la base de donnée fermé."));
+mongoose.connection.on("connected", () =>
+  Logger.info("Connecté à la base de donnée.")
+);
+mongoose.connection.on("open", () =>
+  Logger.info("Connection ouverte à la base de donnée.")
+);
+mongoose.connection.on("disconnected", () =>
+  Logger.error("Déconnecter a la base de donnée.")
+);
+mongoose.connection.on("reconnected", () =>
+  Logger.info("Reconnecté à la base de donnée.")
+);
+mongoose.connection.on("disconnecting", () =>
+  Logger.error("Déconnection à la base de donnée.")
+);
+mongoose.connection.on("close", () =>
+  Logger.info("Connection à la base de donnée fermé.")
+);
 
-
-
-
-mongoose.connect(`${Config.url_database}/${process.env.npm_lifecycle_event == 'test' ? "SLEEPYOWLS_SERVER_TEST" : "SLEEPYOWLS_SERVER_PROD"}`, {
+mongoose.connect(
+  `${Config.url_database}/${
+    process.env.npm_lifecycle_event == "test"
+      ? "SLEEPYOWLS_SERVER_TEST"
+      : "SLEEPYOWLS_SERVER_PROD"
+  }`,
+  {
     useUnifiedTopology: true,
-
-})
+  }
+);
